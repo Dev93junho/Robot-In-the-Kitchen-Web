35bbf2cece1e9f3f6d86742f400bfeef80ab5b7e
jhshin9345@gmail.com <jhshin9345@gmail.com>
2021-10-10 18:59:49 +0900

    Updated Kinematics src

--- utils/KinematicSim.py
+++ utils/KinematicSim.py
@@ -1,11 +1,19 @@
-class KinematicSim(length,joint_num):
-    def link():
+'''
+Inverse Kinematic Solutino of Robot
+Determine the value of each joint to place the arm at a desired position and orientation
+
+Created by Junho Shin in Sep.2021
+'''
+
+import numpy as np
+from numpy import sin, cos, arcsin, tan, pi
+
+class KinematicSim:
+    def __init__(self, *args, **kwargs): # initialize to manipulator
         pass
 
-    def joint():
+    def forward_kinematics():
         pass
-    
-    def shoulder():
-        pass 
 
-    pass
+    def inverse_kinematics(target):
+        pass
