425c96cb8c1d41c9ad678240be2538ca1f869b54
Junho <jhshin9345@gmail.com>
2021-10-11 14:48:54 +0900

    updated kinematicSim.py

--- utils/KinematicSim.py
+++ utils/KinematicSim.py
@@ -7,12 +7,17 @@ Created by Junho Shin in Sep.2021
 
 import numpy as np
 from numpy import sin, cos, arcsin, tan, pi
+import sys
 
 class KinematicSim:
     def __init__(self, *args, **kwargs): # initialize to manipulator
         pass
 
-    ## These modules defined movement method
+    '''
+    Below two modules defined movement method
+    prismatic defined linear motion 
+    revolute defined rotation motion
+    '''
     def prismatic(self):
         pass
 
@@ -20,12 +25,33 @@ class KinematicSim:
         pass
 
 
+    '''
+    Below two modules about 
+    '''
+    def DH_table(self):
+        pass
+
+    def transform_Mat(self):
+        pass
+
+
+    '''
+    Below two modules defined for solving kinematics solution from Kinematics Theory
+    forward_kinematics is solved for End-Effector's target coordination
+    inverse_kinematics module is solved for joint theata from End-Effector's target
+    '''
     def forward_kinematics(self): # fk_module will be return target point coordination
         
         target = -1
         return target
 
     def inverse_kinematics(self): # ik_module will be return joint theta from fk_solution
-        
-        sol = -1
-        return sol
+        try:
+            sol = -1
+            return sol
+
+        except:
+            print("should not solved invers kinematics solution")
+            sys.exit
+            
+
