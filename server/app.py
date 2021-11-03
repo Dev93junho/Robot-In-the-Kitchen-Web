bf5feee47c0d0d2aa77afd6f49ce89481f7a9940
wsl-junho <jhshin9345@gmail.com>
2021-11-03 21:26:09 +0900

    updated flask with livestream

---
+++ server/app.py
@@ -0,0 +1,51 @@
+"""
+This app is contoller for Kitchen Master2
+Written by Junho Shin, 09-2021
+"""
+#coding: utf-8
+
+
+from flask import Flask, render_template, request, session, Response
+import cv2
+from flask_socketio import SocketIO, emit
+
+
+#initialize the Flask app
+app = Flask(__name__) # Flask object instance 
+app.secret_key = "secret"
+socketio = SocketIO(app)
+
+user_no = 1
+
+camera = cv2.VideoCapture(1)
+'''
+for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp'
+for local webcam use cv2.VideoCapture(0)
+'''
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+@app.route('/') #url routing
+def index(): # View function call 
+    return render_template('index.html') # template, seem to user
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(port = 8000, debug=True)
+
+
+
